b37313d275882b2eb65aa5d3e50b54397923f87f
Philipp Strube <pst@kubestack.com>
2020-11-03 12:40:27 +0100

    Disable server side planning as currently required by provider

--- pkg/writer/static.go
+++ pkg/writer/static.go
@@ -3,13 +3,17 @@ package writer
 var staticMain = `terraform {
   required_providers {
     kubernetes-alpha = {
-      source  = "hashicorp/kubernetes-alpha"
+      source = "hashicorp/kubernetes-alpha"
     }
   }
 }
 
 provider "kubernetes-alpha" {
   config_path = "~/.kube/config"
+
+  # set server side planning to false
+  # TODO: remove when this is clear upstream
+  server_side_planning = false
 }
 `
 
