10cc84a6cfed3daef8fba0e5c7eb886a805533d9
Philipp Strube <pst@kubestack.com>
2020-11-09 11:37:26 +0100

    fixup! Use nosetests to run tests and track results per module

--- test.py
+++ test.py
@@ -4,9 +4,8 @@ import time
 import logging
 from os import listdir
 from os.path import isdir, join
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, TimeoutExpired
 from shutil import unpack_archive
-from nose.tools import timed
 
 DISTDIR = "modules/"
 TIMEOUT = 180  # 3 minutes in seconds
@@ -14,24 +13,18 @@ TIMEOUT = 180  # 3 minutes in seconds
 
 def run_cmd(name, cmd, cwd, timeout):
     p = Popen(cmd, cwd=cwd, stdout=PIPE, stderr=PIPE)
-    while True:
-        o = p.stdout.read()
-        if o:
-            logging.error(o.strip().decode("UTF-8"))
+    try:
+        outs, errs = p.communicate(timeout=timeout)
+    except TimeoutExpired:
+        p.kill()
+        outs, errs = p.communicate()
 
-        exit_code = p.poll()
-        if exit_code is not None:
-            break
+    logging.error(outs.strip().decode("UTF-8"))
+    logging.error(errs.strip().decode("UTF-8"))
 
-    if exit_code != 0:
-        e = p.stderr.read()
-        if e:
-            logging.error(e.strip().decode("UTF-8"))
-
-    assert exit_code == 0
+    assert p.returncode == 0
 
 
-@timed(TIMEOUT)
 def run_steps(name, path):
     steps = {
         "init": {"type": "run_cmd",
